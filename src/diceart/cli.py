7e0cdaf2868834e91e03e46570a6c7aec4236f42
Mathis Tailland <mathis.tailland@gmail.com>
2025-05-23 11:43:29 +0200

    fix: patch convert command

--- src/diceart/cli.py
+++ src/diceart/cli.py
@@ -2,12 +2,14 @@ from pathlib import Path
 from typing import Optional
 import typer
 
+from diceart.convert import convert as _convert
+
 app = typer.Typer()
 
 
 @app.command()
 def convert(source: Path, output: Optional[Path] = None):
-    img = convert(source)
+    img = _convert(source)
     # use PNG to avoid compression
     save_path = output if output is not None else source.with_suffix(".diceart.png")
     img.save(save_path)
